0e96324b6735c327d65925e12a15cf1b4a6b0175
Joe <jcaulfield135@gmail.com>
2023-10-26 18:31:10 +0200

    refactor(experimental): add generic `createJsonRpcSubscriptionsApi` for custom APIs

---
+++ packages/rpc-transport/src/apis/subscriptions/subscriptions-api.ts
@@ -0,0 +1,41 @@
+import { IRpcSubscriptionsApi, RpcSubscription } from '../../json-rpc-types';
+import { IRpcApiSubscriptions, RpcSubscriptionsApiConfig } from '../api-types';
+
+export function createJsonRpcSubscriptionsApi<TRpcSubscriptions extends IRpcApiSubscriptions>(
+    config?: RpcSubscriptionsApiConfig
+): IRpcSubscriptionsApi<TRpcSubscriptions> {
+    return new Proxy({} as IRpcSubscriptionsApi<TRpcSubscriptions>, {
+        defineProperty() {
+            return false;
+        },
+        deleteProperty() {
+            return false;
+        },
+        get<TNotificationName extends keyof IRpcSubscriptionsApi<TRpcSubscriptions>>(
+            ...args: Parameters<NonNullable<ProxyHandler<IRpcSubscriptionsApi<TRpcSubscriptions>>['get']>>
+        ) {
+            const [_, p] = args;
+            const notificationName = p.toString() as keyof TRpcSubscriptions as string;
+            return function (
+                ...rawParams: Parameters<
+                    TRpcSubscriptions[TNotificationName] extends CallableFunction
+                        ? TRpcSubscriptions[TNotificationName]
+                        : never
+                >
+            ): RpcSubscription<ReturnType<TRpcSubscriptions[TNotificationName]>> {
+                const params = config?.parametersTransformer
+                    ? config?.parametersTransformer(rawParams, notificationName)
+                    : rawParams;
+                const responseTransformer = config?.responseTransformer
+                    ? config?.responseTransformer<ReturnType<TRpcSubscriptions[TNotificationName]>>
+                    : (rawResponse: unknown) => rawResponse as ReturnType<TRpcSubscriptions[TNotificationName]>;
+                return {
+                    params,
+                    responseTransformer,
+                    subscribeMethodName: notificationName.replace(/Notifications$/, 'Subscribe'),
+                    unsubscribeMethodName: notificationName.replace(/Notifications$/, 'Unsubscribe'),
+                };
+            };
+        },
+    });
+}
--- packages/rpc-transport/src/index.ts
+++ packages/rpc-transport/src/index.ts
@@ -1,5 +1,6 @@
 export * from './apis/api-types';
 export * from './apis/methods/methods-api';
+export * from './apis/subscriptions/subscriptions-api';
 export * from './json-rpc';
 export * from './json-rpc-subscription';
 export * from './transports/http/http-transport';
