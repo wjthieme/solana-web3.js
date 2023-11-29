Xandeum/Xweb3.js

refactor(experimental): use external helper functions instead of introducing new read write methods

packages/codecs-core/src/__tests__/__setup__.ts
@@ -1,21 +1,22 @@
-import { Codec, createCodec } from '../codec';
+import { Codec, encode } from '../codec';
 
-export const b = (s: string) => base16.encode(s);
+export const b = (s: string) => encode(s, base16);
 
-export const base16: Codec<string> = createCodec({
-    description: 'base16',
-    getSize: (value: string) => Math.ceil(value.length / 2),
-    read(bytes, offset = 0) {
+export const base16: Codec<string> = {
+    decode(bytes, offset) {
         const value = bytes.slice(offset).reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
         return [value, bytes.length];
     },
-    write(value: string, bytes, offset) {
+    description: 'base16',
+    encode(value: string, bytes, offset) {
         const matches = value.toLowerCase().match(/.{1,2}/g);
         const hexBytes = matches ? matches.map((byte: string) => parseInt(byte, 16)) : [];
         bytes.set(hexBytes, offset);
         return offset + hexBytes.length;
     },
-});
+    fixedSize: null,
+    variableSize: (value: string) => Math.ceil(value.length / 2),
+};
 
 export const getMockCodec = (
     config: {
@@ -23,12 +24,11 @@ export const getMockCodec = (
         description?: string;
         size?: number | null;
     } = {}
-) =>
-    createCodec({
-        description: config.description ?? 'mock',
-        fixedSize: config.size ?? undefined,
-        getSize: jest.fn().mockReturnValue(config.size ?? 0),
-        maxSize: config.size ?? null,
-        read: jest.fn().mockReturnValue([config.defaultValue ?? '', 0]),
-        write: jest.fn().mockReturnValue(0),
-    });
+) => ({
+    decode: jest.fn().mockReturnValue([config.defaultValue ?? '', 0]),
+    description: config.description ?? 'mock',
+    encode: jest.fn().mockReturnValue(0),
+    fixedSize: config.size ?? null,
+    maxSize: config.size ?? undefined,
+    variableSize: jest.fn().mockReturnValue(config.size ?? 0),
+});

wjthieme/solana-web3.js

packages/codecs-core/src/__tests__/__setup__.ts
@@ -3,12 +3,12 @@ import { Codec, createCodec } from '../codec';
 export const b = (s: string) => base16.encode(s);
 
 export const base16: Codec<string> = createCodec({
-    description: 'base16',
-    getSize: (value: string) => Math.ceil(value.length / 2),
-    read(bytes, offset = 0) {
+    fixedSize: null,
+    read(bytes, offset) {
         const value = bytes.slice(offset).reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
         return [value, bytes.length];
     },
+    variableSize: (value: string) => Math.ceil(value.length / 2),
     write(value: string, bytes, offset) {
         const matches = value.toLowerCase().match(/.{1,2}/g);
         const hexBytes = matches ? matches.map((byte: string) => parseInt(byte, 16)) : [];
@@ -25,10 +25,13 @@ export const getMockCodec = (
     } = {}
 ) =>
     createCodec({
-        description: config.description ?? 'mock',
-        fixedSize: config.size ?? undefined,
-        getSize: jest.fn().mockReturnValue(config.size ?? 0),
-        maxSize: config.size ?? null,
+        fixedSize: config.size ?? null,
+        maxSize: config.size ?? undefined,
         read: jest.fn().mockReturnValue([config.defaultValue ?? '', 0]),
+        variableSize: jest.fn().mockReturnValue(config.size ?? 0),
         write: jest.fn().mockReturnValue(0),
-    });
+    }) as Codec<unknown> & {
+        readonly read: jest.Mock;
+        readonly variableSize: jest.Mock;
+        readonly write: jest.Mock;
+    };